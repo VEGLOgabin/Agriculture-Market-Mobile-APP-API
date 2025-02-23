1bdcf337c08d828eaae7206ea2fa1d5c1c5985df
VEGLOgabin <houetchenougabin@gmail.com>
2025-02-23 15:53:50 +0100

    API

--- api/models.py
+++ api/models.py
@@ -22,7 +22,7 @@ class UserManager(BaseUserManager):
             **kwargs
         )
         user.set_password(password)
-        user.save(using=self._db)
+        user.save()
         return user
         
 
